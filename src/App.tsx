9a20ec3ca9436834db31c652969edca76c203668
isira-perera <133723716+isira-perera@users.noreply.github.com>
2023-05-25 09:36:51 -0500

    Finished Navbar, set up for building store page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { Routes, Route } from "react-router-dom"
-import { Container } from "react-bootstrap"
+import { Container, Nav } from "react-bootstrap"
 import { Home } from "./pages/home"
 import { About } from "./pages/about"
 import { Store } from "./pages/store"
+import { Navbar } from "./components/Navbar"
 
 
 console.log("ppop")
 
 function App() {
-  return <Container>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </Container>
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  )
 }
 
 
